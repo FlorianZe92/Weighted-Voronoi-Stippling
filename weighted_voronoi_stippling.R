3907820b8e14836ee645b1f042ed65fea8b48798
FlorianZe92 <zeidler.florian@yahoo.de>
2022-06-26 12:04:35 +0200

    Aktualisierung

--- weighted_voronoi_stippling.R
+++ weighted_voronoi_stippling.R
@@ -30,9 +30,11 @@ stipple <- function(im,nDots=1e3,gamma=1,nSteps=5)
 {
   dens <- (1-im)^gamma
   xy <- sample(nPix(im),nDots,replace=TRUE,prob=dens) %>% coord.index(im,.) %>% select(x,y)
-  for (i in 2:nSteps)
+  
+  for (i in 1:nSteps)
   {
     xy <- cvt(xy,dens)
+
   }
   xy
 }
@@ -40,13 +42,13 @@ stipple <- function(im,nDots=1e3,gamma=1,nSteps=5)
 ##Use of Algorithm in the following lines:
 
 ##Load Image and safe it grayscaled
-im <- load.image("Input/Gecko.png")
+im <- load.image("Input/Zebra.jpg")
 im <- grayscale(im) 
 
 ##Stippling Algorithm with the use of weighted centroid coronoi diagrams
-out <- stipple(im,nDots=10000,gamma=2,nSteps=5)
+out <- stipple(im,nDots=50000,gamma=2,nSteps=2)
 
 ##Save image as pdf
-pdf("Output/Gecko.pdf") 
-plot(out,ylim=c(height(im),1),main=paste("Gecko mit 10000 Punkten und 5 Voronoi Durchläufen"), xlim=c(1, width(im)), cex=.1,pch=19,axes=FALSE,xlab="",ylab="")
+pdf("Output/Zebra.pdf") 
+plot(out,ylim=c(height(im),1),main=paste("Zebra mit 50.000 Punkten und 2 Voronoi Durchläufen"), xlim=c(1, width(im)), cex=.1,pch=19,axes=FALSE,xlab="",ylab="")
 dev.off() 
